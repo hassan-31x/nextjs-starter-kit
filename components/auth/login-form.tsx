c3cca81f33fa5177dae98960ca5bfeb710abbe60
hassan-31x <muhammadhassanchannel786@gmail.com>
2024-07-23 23:41:38 +0500

    feat: setup dummy server action for login

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,30 +1,43 @@
 "use client"
 
-import React from 'react'
+import React, { startTransition, useState, useTransition } from 'react'
 import CardWrapper from './card-wrapper'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
-import { loginSchema } from '@/schemas'
+import { LoginSchema } from '@/schemas'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import FormError from '@/components/form-error'
 import FormSuccess from '@/components/form-success'
+import { login } from '@/actions/login'
 
 type Props = {}
 
 const LoginForm = (props: Props) => {
-  const form = useForm<z.infer<typeof loginSchema>>({
-    resolver: zodResolver(loginSchema),
+  const [error, setError] = useState<string | undefined>("")
+  const [success, setSuccess] = useState<string | undefined>("")
+  const [isPending, setIsPending] = useTransition() //automatically changes state on revalidatePath with server action
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: '',
       password: '',
     }
   })
 
-  const onSubmit = (values: z.infer<typeof loginSchema>) => {
-    console.log(values)
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError("")
+    setSuccess("")
+
+    startTransition(() => {
+      login(values).then((data) => {
+        setError(data.error)
+        setSuccess(data.success)
+      })
+    })
   }
 
   return (
@@ -74,8 +87,8 @@ const LoginForm = (props: Props) => {
             >
             </FormField>
           </div>
-          <FormError message={'Email Invalid!'} />
-          <FormSuccess message={'Email Invalid!'} />
+          <FormError message={error} />
+          <FormSuccess message={success} />
           <Button
             type='submit'
             className='w-full'
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-export const loginSchema = z.object({
+export const LoginSchema = z.object({
   email: z.string().email().min(1, {
     message: "Email is required"
   }),
