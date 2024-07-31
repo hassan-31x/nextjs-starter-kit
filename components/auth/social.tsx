ea0211c862290d2e1e8585ad453e17c4af30bde6
hassan-31x <muhammadhassanchannel786@gmail.com>
2024-07-31 22:33:31 +0500

    social login working

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,14 +1,20 @@
 "use client"
 
-import React from 'react'
+import { signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
-import { Button } from '@/components/ui/button'
 import { FaGithub } from 'react-icons/fa'
+
+import { Button } from '@/components/ui/button'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
 type Props = {}
 
 const AuthSocial = (props: Props) => {
-  
+  const handleClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+  }
 
   return (
     <div className='flex items-center w-full gap-x-2'>
@@ -16,7 +22,7 @@ const AuthSocial = (props: Props) => {
         size='lg'
         className='w-full'
         variant='outline'
-        onClick={() => console.log('Facebook')}
+        onClick={() => handleClick('google')}
       >
         <FcGoogle className='h-5 w-5' />
       </Button>
@@ -24,7 +30,7 @@ const AuthSocial = (props: Props) => {
         size='lg'
         className='w-full'
         variant='outline'
-        onClick={() => console.log('Facebook')}
+        onClick={() => handleClick('github')}
       >
         <FaGithub className='h-5 w-5' />
       </Button>
