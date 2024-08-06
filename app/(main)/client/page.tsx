d16ca03e7cafea693b206b9f70f6f4b98add9318
hassan-31x <muhammadhassanchannel786@gmail.com>
2024-08-06 21:47:31 +0500

    feat: user json parsed

--- app/(main)/client/page.tsx
+++ app/(main)/client/page.tsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react";
 const ClientPage = () => {
   const { data: session } = useSession();
   
-  return <div>{JSON.stringify(session?.user)}</div>;
+  return <pre>{JSON.stringify(session?.user, null, 2)}</pre>;
 };
 
 export default ClientPage;
--- app/(main)/server/page.tsx
+++ app/(main)/server/page.tsx
@@ -4,7 +4,7 @@ const ServerPage = async () => {
   const session = await auth(); // server function, hence can also be used in api routes
   const user = session?.user;
 
-  return <div>{JSON.stringify(user)}</div>;
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
 };
 
 export default ServerPage;
